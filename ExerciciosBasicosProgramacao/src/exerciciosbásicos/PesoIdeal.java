9e06278dac0a3fc636e01f149222dacccbf9df7e
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-14 18:26:10 -0300

    Alterações na documentação e na estrutura de decisão

--- ExerciciosBasicosProgramacao/src/exerciciosbásicos/PesoIdeal.java
+++ ExerciciosBasicosProgramacao/src/exerciciosbásicos/PesoIdeal.java
@@ -1,12 +1,18 @@
 package exerciciosbásicos;
 import java.util.Scanner;
 
+/* autor: Gabriel Jorge
+ * problema: Crie um algoritmo que receba altura
+ *  e sexo de uma pessoa e calcule o peso ideal
+ */
+
 public class PesoIdeal {
 	public static void main (String[]args) {
 		Scanner input = new Scanner(System.in);
 		double altura,pesoid;
 		String nome;
 		int sexo;
+		System.out.println("=============Cálculo de peso ideal: =============");
 		System.out.println("Digite sua altura: ");
 		altura = input.nextDouble();
 		System.out.println("Digite seu nome: ");
@@ -17,10 +23,12 @@ public class PesoIdeal {
 			pesoid = (72.7 * altura) - 58;
 			System.out.printf("%s, seu peso ideal é %.2f kg \n",nome,pesoid);
 		}
-		else {
+		else if (sexo == 2) {
 			pesoid = (62.1 * altura) - 44.7;
 			System.out.printf("%s, seu peso ideal é %.2f kg \n", nome, pesoid);
 		}
+		else 
+			System.out.println("Opção inválida.");
 	}
 
 }
