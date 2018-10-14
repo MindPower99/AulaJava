3543a822afd207173c04884f81108bae1be1c16a
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-14 19:07:24 -0300

    Alterações no código e na documentação

--- ExerciciosBasicosProgramacao/src/exerciciosbásicos/ValidarSenha.java
+++ ExerciciosBasicosProgramacao/src/exerciciosbásicos/ValidarSenha.java
@@ -1,15 +1,26 @@
 package exerciciosbásicos;
 import java.util.Scanner;
-
+/*
+ * autor: Gabriel Nascimento
+ * 		problema: Criar um programa que recebe
+ *  	uma senha, e se for 'asdfg', retorne
+ *   	'senha válida'.
+ *   	 Caso contrário retorne senha inválida
+ *   	Dificultando: Crie um programa que registre uma senha,
+ *   	 peça ela e verifique se está correto.
+ */
 public class ValidarSenha {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String se = "asdfg";
+		String se;
 		String senha;
 		System.out.println("========= Validação de Senha: =========");
 		System.out.println("Digite a senha: ");
 		senha = input.next();
+		System.out.println("Digite a senha novamente: ");
+		se = input.next();
+		
 		
 		if (!senha.equals(se)) {
 			System.out.println("Senha inválida.");
