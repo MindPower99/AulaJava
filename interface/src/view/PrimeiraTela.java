4ca6b69367adba06ef4cf45aeecc284cebf8988a
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-10-23 14:31:26 -0200

    arrumando alguns erros e adicionando radioButton

--- interface/src/view/PrimeiraTela.java
+++ interface/src/view/PrimeiraTela.java
@@ -16,6 +16,7 @@ import javax.swing.text.MaskFormatter;
 
 
 public class PrimeiraTela extends JFrame {
+	
 	JLabel lblNome = new JLabel("Nome: ");
 	JTextField txtNome = new JTextField();
 	JLabel lblEndereco = new JLabel("Endereço: ");
@@ -54,77 +55,87 @@ public class PrimeiraTela extends JFrame {
 		Container paine = this.getContentPane();
 		
 		paine.add(lblNome);
-		lblNome.setBounds(20,20,100,20);
+		lblNome.setBounds(30,20,100,20);
 		paine.add(txtNome);
-		txtNome.setBounds(130,20,200,20);
+		txtNome.setBounds(140,20,200,20);
 		
 		paine.add(lblEndereco);
-		lblEndereco.setBounds(20,50,100,20);
+		lblEndereco.setBounds(30,50,100,20);
 		paine.add(txtEndereco);
-		txtEndereco.setBounds(130,50,200,20);
+		txtEndereco.setBounds(140,50,200,20);
 		
 		paine.add(lblCidade);
-		lblCidade.setBounds(20,80,100,20);
+		lblCidade.setBounds(30,80,100,20);
 		paine.add(txtCidade);
-		txtCidade.setBounds(130,80,200,20);
+		txtCidade.setBounds(140,80,200,20);
 		
 		paine.add(lblCEP);
-		lblCEP.setBounds(20,110,100,20);
+		lblCEP.setBounds(30,110,100,20);
 		paine.add(txtCEP);
-		txtCEP.setBounds(130,110,200,20);
+		txtCEP.setBounds(140,110,200,20);
 		
 		paine.add(lblEstado);
-		lblEstado.setBounds(20,140,100,20);
+		lblEstado.setBounds(30,140,100,20);
 		paine.add(cmbEst);
-		cmbEst.setBounds(130,140,200,20);
-		cmbEst.addItem("AC");		cmbEst.addItem("AL");
-		cmbEst.addItem("AP");		cmbEst.addItem("AM");
-		cmbEst.addItem("BA");		cmbEst.addItem("CE");
-		cmbEst.addItem("DF");		cmbEst.addItem("ES");
-		cmbEst.addItem("GO");		cmbEst.addItem("MA");
-		cmbEst.addItem("MT");		cmbEst.addItem("MS");
-		cmbEst.addItem("MG");		cmbEst.addItem("PA");
-		cmbEst.addItem("PB");		cmbEst.addItem("PR");
-		cmbEst.addItem("PE");		cmbEst.addItem("PI");
-		cmbEst.addItem("RJ");		cmbEst.addItem("RN");
-		cmbEst.addItem("RS");		cmbEst.addItem("RO");
-		cmbEst.addItem("RR");		cmbEst.addItem("SC");
-		cmbEst.addItem("SP");		cmbEst.addItem("SE");
-		cmbEst.addItem("TO");
+		cmbEst.setBounds(140,140,200,20);
+		cmbEst.addItem("Acre");		cmbEst.addItem("Alagoas");
+		cmbEst.addItem("Amapá");		cmbEst.addItem("Amapá");
+		cmbEst.addItem("Bahia");		cmbEst.addItem("Ceará");
+		cmbEst.addItem("Distrito Federal");		cmbEst.addItem("Espírito Santo");
+		cmbEst.addItem("Goiás");		cmbEst.addItem("Maranhão");
+		cmbEst.addItem("Mato Grosso");		cmbEst.addItem("Mato Grosso do Sul");
+		cmbEst.addItem("Minas Gerais");		cmbEst.addItem("Pará");
+		cmbEst.addItem("Paraíba");		cmbEst.addItem("Paraná");
+		cmbEst.addItem("Pernambuco");		cmbEst.addItem("Piauí");
+		cmbEst.addItem("Rio de Janeiro");		cmbEst.addItem("Rio Grande do Norte");
+		cmbEst.addItem("Rio Grande do Sul");		cmbEst.addItem("Rondônia");
+		cmbEst.addItem("Roraima");		cmbEst.addItem("Santa Catarina");
+		cmbEst.addItem("São Paulo");		cmbEst.addItem("Sergipe");
+		cmbEst.addItem("Tocantins");
 		
 		paine.add(lblBairro);
-		lblBairro.setBounds(20,170,100,20);
+		lblBairro.setBounds(30,170,100,20);
 		paine.add(txtBairro);
-		txtBairro.setBounds(130,170,200,20);
+		txtBairro.setBounds(140,170,200,20);
 		
 		paine.add(lblTelefone);
-		lblBairro.setBounds(20,200,100,20);
+		lblTelefone.setBounds(30,200,100,20);
 		paine.add(txtTelefone);
-		txtBairro.setBounds(130,200,200,20);
+		txtTelefone.setBounds(140,200,200,20);
 		
 		paine.add(lblCelular);
-		lblBairro.setBounds(20,230,100,20);
+		lblCelular.setBounds(30,230,100,20);
 		paine.add(txtCelular);
-		txtBairro.setBounds(130,230,200,20);
+		txtCelular.setBounds(140,230,200,20);
 		
 		paine.add(lblRG);
-		lblRG.setBounds(20,260,100,20);
+		lblRG.setBounds(30,260,100,20);
 		paine.add(txtRG);
-		txtRG.setBounds(130,260,100,20);
+		txtRG.setBounds(140,260,100,20);
 		
 		paine.add(lblCPF);
-		lblRG.setBounds(20,290,100,20);
+		lblCPF.setBounds(30,290,100,20);
 		paine.add(txtCPF);
-		txtRG.setBounds(130,290,100,20);
+		txtCPF.setBounds(140,290,100,20);
+		
+		paine.add(lblSexo);
+		lblSexo.setBounds(30,320,100,20);
+		grupo.add(rbdSexo[0]);
+		grupo.add(rbdSexo[1]);
+		rbdSexo[0].setBounds(140,320,100,20);
+		paine.add(rbdSexo[0]);
+		rbdSexo[1].setBounds(230,320,100,20);
+		paine.add(rbdSexo[1]);
+		
 		
 		paine.add(btnSalvar);
-		btnSalvar.setBounds(130,320,100,20);
+		btnSalvar.setBounds(90,390,100,20);
 		
 		paine.add(btnImprimir);
-		btnSalvar.setBounds(130,320,100,20);
+		btnImprimir.setBounds(200,390,100,20);
 		
 		this.setLayout(null);
-		this.setSize(400,400);
+		this.setSize(420,500);
 		//this.setResizable(0);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
