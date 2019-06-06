0edd381cf5218971267a05747375209bcb19590e
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-06-06 19:55:20 -0300

    Fazendo a conexão / incompleto

--- ExemploBancoDeDados/src/controller/JdbUtil.java
+++ ExemploBancoDeDados/src/controller/JdbUtil.java
@@ -1,5 +1,27 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class JdbUtil {
+	private static String connectionDriverClass="com.mysql.jdbc.Driver";
+	private static String connectionUrl = "jdbc:mysql://localhost/java";
+	private static String connectionUsername="root";
+	private static String connectionPassword="";
+	private static Connection conn;
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		if(jdbUtil.conn != null) {
+			return jdbUtil.conn;
+		}
+		else {
+			Class.forName(connectionDriverClass);
+			return DriverManager.getConnection(
+					jdbUtil.connectionUrl,
+					jdbUtil.connectionUsername,
+					jdbUtil.connectionPassword);
+					
+		}
+	}
 
 }
